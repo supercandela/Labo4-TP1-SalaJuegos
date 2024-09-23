eae4bd05b5c40eddc6f1fc168d54bdf6aaef4a79
supercandela <supercandela@gmail.com>
2024-09-23 20:28:19 -0300

    ahorcado casi terminado

--- src/app/modules/juegos/componentes/ahorcado/ahorcado.component.ts
+++ src/app/modules/juegos/componentes/ahorcado/ahorcado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ahorcado',
@@ -7,60 +8,513 @@ import { Component } from '@angular/core';
 })
 export class AhorcadoComponent {
   puntaje: number = 0;
+  palabraAAdivinar: string = ''; // La palabra que hay que adivinar
+  palabraMostrada: string = ''; // La palabra que se muestra con guiones
+  errores: number = 0; // Numero de errores cometidos
+  maxerrores: number = 6; // Máximo numero de errores permitidos
+  alfabeto: string[] = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split(''); // Alfabeto
+  letrasUsadas: string[] = []; // Letras que ya fueron seleccionadas
 
-  secretWord: string = 'angular'; // La palabra que hay que adivinar
-  displayedWord: string = ''; // La palabra que se muestra con guiones
-  mistakes: number = 0; // Número de errores cometidos
-  maxMistakes: number = 6; // Máximo número de errores permitidos
-  alphabet: string[] = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split(''); // Alfabeto
-  usedLetters: string[] = []; // Letras que ya fueron seleccionadas
+  palabras: string[] = [
+    'cielo',
+    'mariposa',
+    'ventana',
+    'camino',
+    'espejo',
+    'montaña',
+    'manzana',
+    'libro',
+    'amigo',
+    'esperanza',
+    'noche',
+    'fuego',
+    'tierra',
+    'sol',
+    'agua',
+    'perro',
+    'gato',
+    'ciudad',
+    'bosque',
+    'palabra',
+    'sueño',
+    'silencio',
+    'corazon',
+    'mundo',
+    'felicidad',
+    'sonrisa',
+    'estrella',
+    'lluvia',
+    'fuerza',
+    'luz',
+    'arbol',
+    'casa',
+    'puente',
+    'piedra',
+    'flor',
+    'rojo',
+    'verde',
+    'azul',
+    'negro',
+    'blanco',
+    'dulce',
+    'fuerte',
+    'grande',
+    'pequeño',
+    'alto',
+    'largo',
+    'ancho',
+    'recto',
+    'redondo',
+    'claro',
+    'oscuro',
+    'nuevo',
+    'viejo',
+    'frio',
+    'caliente',
+    'tibio',
+    'suave',
+    'duro',
+    'feliz',
+    'triste',
+    'rapido',
+    'lento',
+    'peligroso',
+    'seguro',
+    'sabio',
+    'tonto',
+    'fresco',
+    'serio',
+    'vivo',
+    'muerto',
+    'lento',
+    'ligero',
+    'fino',
+    'grueso',
+    'firme',
+    'blando',
+    'libre',
+    'atado',
+    'joven',
+    'anciano',
+    'dorado',
+    'plateado',
+    'brillante',
+    'oscuro',
+    'limpio',
+    'sucio',
+    'lleno',
+    'vacio',
+    'seco',
+    'mojado',
+    'rico',
+    'pobre',
+    'honesto',
+    'mentiroso',
+    'valiente',
+    'miedoso',
+    'justo',
+    'injusto',
+    'sabroso',
+    'desagradable',
+    'amargo',
+    'picante',
+    'alegre',
+    'enojado',
+    'enfermo',
+    'sano',
+    'hermoso',
+    'feo',
+    'caro',
+    'barato',
+    'famoso',
+    'desconocido',
+    'pesado',
+    'ligero',
+    'facil',
+    'dificil',
+    'amable',
+    'cruel',
+    'gracioso',
+    'serio',
+    'tranquilo',
+    'ruidoso',
+    'comodo',
+    'incomodo',
+    'corto',
+    'largo',
+    'delgado',
+    'gordo',
+    'estrecho',
+    'ancho',
+    'profundo',
+    'superficial',
+    'antiguo',
+    'moderno',
+    'fragil',
+    'resistente',
+    'frontal',
+    'trasero',
+    'proximo',
+    'lejano',
+    'abierto',
+    'cerrado',
+    'facil',
+    'dificil',
+    'importante',
+    'insignificante',
+    'perfecto',
+    'imperfecto',
+    'recto',
+    'curvado',
+    'redondo',
+    'cuadrado',
+    'triangular',
+    'sincero',
+    'hipocrita',
+    'orgulloso',
+    'humilde',
+    'valioso',
+    'insignificante',
+    'contento',
+    'disgustado',
+    'maduro',
+    'inmaduro',
+    'lento',
+    'veloz',
+    'constante',
+    'variable',
+    'necesario',
+    'innecesario',
+    'fuerte',
+    'debil',
+    'alegre',
+    'triste',
+    'esperanzado',
+    'desesperado',
+    'optimista',
+    'pesimista',
+    'preciso',
+    'vago',
+    'concreto',
+    'abstracto',
+    'real',
+    'irreal',
+    'facil',
+    'complicado',
+    'amplio',
+    'estrecho',
+    'comodo',
+    'incomodo',
+    'firme',
+    'flexible',
+    'despacio',
+    'deprisa',
+    'seguro',
+    'inseguro',
+    'util',
+    'inutil',
+    'agradable',
+    'desagradable',
+    'bonito',
+    'feo',
+    'inteligente',
+    'ignorante',
+    'atento',
+    'distraido',
+    'complicado',
+    'sencillo',
+    'amplio',
+    'reducido',
+    'conocido',
+    'desconocido',
+    'barato',
+    'caro',
+    'normal',
+    'extraño',
+    'habitual',
+    'infrecuente',
+    'duro',
+    'blando',
+    'flexible',
+    'rigido',
+    'agradable',
+    'molesto',
+    'importante',
+    'trivial',
+    'necesario',
+    'prescindible',
+    'peligroso',
+    'seguro',
+    'activo',
+    'pasivo',
+    'creativo',
+    'monotono',
+    'logico',
+    'irracional',
+    'feliz',
+    'desgraciado',
+    'brillante',
+    'opaco',
+    'rico',
+    'pobre',
+    'optimista',
+    'pesimista',
+    'progresivo',
+    'regresivo',
+    'alegre',
+    'melancolico',
+    'duro',
+    'suave',
+    'tranquilo',
+    'nervioso',
+    'paciente',
+    'impaciente',
+    'veloz',
+    'lento',
+    'atento',
+    'distraido',
+    'clasico',
+    'moderno',
+    'aburrido',
+    'emocionante',
+    'positivo',
+    'negativo',
+    'puntual',
+    'tardio',
+    'brusco',
+    'suave',
+    'luminoso',
+    'oscuro',
+    'energico',
+    'agotado',
+    'lucido',
+    'confuso',
+    'sabroso',
+    'insipido',
+    'amargo',
+    'dulce',
+    'claro',
+    'turbio',
+    'entusiasmado',
+    'indiferente',
+    'calmado',
+    'alterado',
+    'capaz',
+    'inutil',
+    'ligero',
+    'pesado',
+    'distante',
+    'cercano',
+    'seco',
+    'humedo',
+    'fresco',
+    'caliente',
+    'frio',
+    'tibio',
+    'mojado',
+    'seco',
+    'ligero',
+    'fuerte',
+    'tierno',
+    'tosco',
+    'rapido',
+    'lento',
+    'horrible',
+    'maravilloso',
+    'bello',
+    'feo',
+    'normal',
+    'extraordinario',
+    'extraño',
+    'corriente',
+    'cansado',
+    'descansado',
+    'silencioso',
+    'bullicioso',
+    'ruidoso',
+    'callado',
+    'oscuro',
+    'luminoso',
+    'sereno',
+    'tormentoso',
+    'tranquilo',
+    'alterado',
+    'vivo',
+    'muerto',
+    'sano',
+    'enfermo',
+    'saludable',
+    'malo',
+    'bueno',
+    'perfecto',
+    'defectuoso',
+    'limpio',
+    'sucio',
+    'ordenado',
+    'desordenado',
+    'hermoso',
+    'feo',
+    'apasionado',
+    'indiferente',
+    'ardiente',
+    'frio',
+    'valiente',
+    'cobarde',
+    'amable',
+    'descortes',
+    'sociable',
+    'timido',
+    'prudente',
+    'temerario',
+    'sabio',
+    'ignorante',
+    'alegre',
+    'triste',
+    'inteligente',
+    'tonto',
+    'habil',
+    'torpe',
+    'serio',
+    'gracioso',
+    'maduro',
+    'infantil',
+    'generoso',
+    'tacaño',
+    'humilde',
+    'orgulloso',
+    'simpatico',
+    'antipatico',
+    'optimista',
+    'pesimista',
+    'nervioso',
+    'tranquilo',
+    'decidido',
+    'indeciso',
+    'detallista',
+    'despreocupado',
+    'metodico',
+    'improvisador',
+    'educado',
+    'maleducado',
+    'honesto',
+    'mentiroso',
+    'justo',
+    'injusto',
+    'pacifico',
+    'agresivo',
+    'solidario',
+    'egoista',
+    'tolerante',
+    'intolerante',
+    'coherente',
+    'incoherente',
+    'constante',
+    'inconstante',
+    'valiente',
+    'miedoso',
+    'prudente',
+    'temerario',
+    'educado',
+    'grosero',
+    'tranquilo',
+    'nervioso',
+    'alegre',
+    'melancolico',
+    'cordial',
+    'frio',
+    'positivo',
+    'negativo',
+    'creativo',
+    'conformista',
+    'entusiasta',
+    'apatico',
+    'amable',
+    'descortes',
+    'ingenioso',
+    'aburrido',
+    'discreto',
+    'indiscreto',
+    'abierto',
+    'cerrado',
+    'fresco',
+    'agotado',
+    'sano',
+    'enfermo',
+    'valiente',
+    'cobarde',
+    'diligente',
+    'perezoso',
+    'curioso',
+  ];
 
-  hangmanImages: string[] = [
+  imagenesAhorcado: string[] = [
     '../../../../../assets/ahorcado-0.png', // Imagen inicial sin errores
     '../../../../../assets/ahorcado-1.png', // Imagen con 1 error
     '../../../../../assets/ahorcado-2.png', // Imagen con 2 errores
     '../../../../../assets/ahorcado-3.png', // Imagen con 3 errores
     '../../../../../assets/ahorcado-4.png', // Imagen con 4 errores
     '../../../../../assets/ahorcado-5.png', // Imagen con 5 errores
-    '../../../../../assets/ahorcado-7.png',  // Imagen final, ahorcado completo
+    '../../../../../assets/ahorcado-7.png', // Imagen final, ahorcado completo
   ];
 
-  hangmanImage: string = this.hangmanImages[0]; // Imagen actual del ahorcado
+  imagenAhorcadoAMostrar: string = this.imagenesAhorcado[0]; // Imagen actual del ahorcado
 
   constructor() {
-    this.resetGame();
+    this.reiniciarPartida();
+  }
+
+  obtenerPalabra(): string {
+    const randomIndex = Math.floor(Math.random() * this.palabras.length);
+    return this.palabras[randomIndex];
   }
 
   // Inicializa el juego
-  resetGame() {
-    this.mistakes = 0;
-    this.hangmanImage = this.hangmanImages[0];
-    this.usedLetters = [];
-    this.displayedWord = this.secretWord.replace(/./g, '_');
+  reiniciarPartida() {
+    this.palabraAAdivinar = this.obtenerPalabra(); // Selecciona una palabra aleatoria
+    this.errores = 0;
+    this.imagenAhorcadoAMostrar = this.imagenesAhorcado[0];
+    this.letrasUsadas = [];
+    this.palabraMostrada = this.palabraAAdivinar.replace(/./g, '_');
   }
 
- // Método para manejar el intento del jugador
- guessLetter(letter: string) {
-  this.usedLetters.push(letter); // Marcar la letra como usada
+  // Método para manejar el intento del jugador
+  arriesgarLetra(letra: string) {
+    this.letrasUsadas.push(letra); // Marcar la letra como usada
 
-  if (this.secretWord.toUpperCase().includes(letter)) {
-    let updatedWord = '';
-    for (let i = 0; i < this.secretWord.length; i++) {
-      if (this.secretWord[i].toUpperCase() === letter) {
-        updatedWord += this.secretWord[i];
-      } else {
-        updatedWord += this.displayedWord[i];
+    if (this.palabraAAdivinar.toUpperCase().includes(letra)) {
+      let updatedWord = '';
+      for (let i = 0; i < this.palabraAAdivinar.length; i++) {
+        if (this.palabraAAdivinar[i].toUpperCase() === letra) {
+          updatedWord += this.palabraAAdivinar[i];
+        } else {
+          updatedWord += this.palabraMostrada[i];
+        }
       }
+      this.palabraMostrada = updatedWord.toUpperCase();
+    } else {
+      this.errores++;
+      this.imagenAhorcadoAMostrar = this.imagenesAhorcado[this.errores]; // Actualiza la imagen del ahorcado
     }
-    this.displayedWord = updatedWord;
-  } else {
-    this.mistakes++;
-    this.hangmanImage = this.hangmanImages[this.mistakes]; // Actualiza la imagen del ahorcado
-  }
 
-  // Chequear si el jugador ha perdido
-  if (this.mistakes >= this.maxMistakes) {
-    alert('¡Perdiste! La palabra era: ' + this.secretWord);
+    // Chequear si el jugador ha perdido
+    if (this.errores >= this.maxerrores) {
+      Swal.fire({
+        icon: 'error',
+        title: '¡Perdiste!',
+        text: 'La palabra era: ' + this.palabraAAdivinar,
+      });
+    }
+
+    // Cuando el jugador gana
+    if (this.palabraMostrada == this.palabraAAdivinar.toUpperCase()) {
+      for (let i = 0; i < this.alfabeto.length; i++) {
+        this.letrasUsadas.push(this.alfabeto[i]);
+      }
+      Swal.fire({
+        icon: 'success',
+        title: '¡Ganaste!',
+        text: 'Adivinaste la palabra correctamente.',
+      });
+    }
   }
-}
 }
